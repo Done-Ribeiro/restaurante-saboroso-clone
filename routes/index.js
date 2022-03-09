f0d1702fe7290b09046245becdf9bd8a2b7eb3d5
Done Ribeiro <www.done_@hotmail.com>
2022-03-08 22:59:50 -0300

    organizing content with include

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results) => {
-    if (err) {
-      console.log(err);
-    }
-
+  menus.getMenus().then(results => {
     res.render('index', {
       title: 'Restaurante Saboroso!',
       menus: results
@@ -27,10 +22,13 @@ router.get('/contacts', function (req, res, next) {
 });
 
 router.get('/menus', function (req, res, next) {
-  res.render('menus', {
-    title: 'Menus - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu!'
+  menus.getMenus().then(results => {
+    res.render('menus', {
+      title: 'Menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    });
   });
 });
 
