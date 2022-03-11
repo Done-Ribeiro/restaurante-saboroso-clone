4d42ffb3ccb34b8a5e5724eff11c4992fd6bcf9b
Done Ribeiro <www.done_@hotmail.com>
2022-03-11 16:34:59 -0300

    admin - home dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -29,7 +29,13 @@ router.get('/logout', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-  res.render('admin/index', admin.getParams(req));
+  admin.dashboard().then(data => {
+    res.render('admin/index', admin.getParams(req, {
+      data
+    }));
+  }).catch(err => {
+    console.error(err);
+  });
 });
 
 router.get('/login', function (req, res, next) {
