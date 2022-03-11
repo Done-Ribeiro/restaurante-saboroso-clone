6919f34abf7de1b8ca261c1a944c93f6ce049616
Done Ribeiro <www.done_@hotmail.com>
2022-03-11 17:05:05 -0300

    admin - 'menu/' read data and show

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var users = require('./../inc/users');
 var admin = require('./../inc/admin');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 // configurando middleware (nivel de roteador) - autenticacao login,  rota -> '/login'
@@ -74,7 +75,11 @@ router.get('/emails', function (req, res, next) {
 });
 
 router.get('/menus', function (req, res, next) {
-  res.render('admin/menus', admin.getParams(req));
+  menus.getMenus().then(data => {
+    res.render('admin/menus', admin.getParams(req, {
+      data
+    }));
+  });
 });
 
 router.get('/reservations', function (req, res, next) {
