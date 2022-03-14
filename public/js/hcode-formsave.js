298924821ea27f5eaa9cdb0c3e84bbe8e3d750ed
Done Ribeiro <www.done_@hotmail.com>
2022-03-14 12:01:11 -0300

    plugin created - form save

---
+++ public/js/hcode-formsave.js
@@ -0,0 +1,24 @@
+// add um prototype ao obj do DOM do formulario, e add um recurso pra ele
+HTMLFormElement.prototype.save = function () {
+  let form = this;
+
+  return new Promise((resolve, reject) => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+
+      let formData = new FormData(form);
+
+      fetch(form.action, {
+        method: form.method,
+        body: formData
+      })
+        .then(response => response.json())
+        .then(json => {
+          resolve(json);
+
+        }).catch(err => {
+          reject(err);
+        });
+    });
+  });
+}
