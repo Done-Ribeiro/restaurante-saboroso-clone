e98cd4f69f5e0b0ac986e96650f6f7dbea2f94d5
Done Ribeiro <www.done_@hotmail.com>
2022-03-14 18:47:01 -0300

    reservations - add momentjs to show date format

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,10 @@ var users = require('./../inc/users');
 var admin = require('./../inc/admin');
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
+var moment = require('moment');
 var router = express.Router();
+
+moment.locale('pt-BR');
 
 // configurando middleware (nivel de roteador) - autenticacao login,  rota -> '/login'
 router.use(function (req, res, next) {
@@ -103,7 +106,8 @@ router.get('/reservations', function (req, res, next) {
   reservations.getReservations().then(data => {
     res.render('admin/reservations', admin.getParams(req, {
       date: {},
-      data
+      data,
+      moment
     }));
   });
 });
