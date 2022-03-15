27c8e26811e50e1a7c04d5c464120bb82ee7f9ff
Done Ribeiro <www.done_@hotmail.com>
2022-03-15 18:13:19 -0300

    users - add listeners

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -30,13 +30,15 @@ class HcodeGrid {
     this.options = Object.assign({
       formCreate: '#modal-create form',
       formUpdate: '#modal-update form',
-      btnUpdate: '.btn-update',
-      btnDelete: '.btn-delete',
+      btnUpdate: 'btn-update',
+      btnDelete: 'btn-delete',
       onUpdateLoad: (form, name, data) => {
         let input = form.querySelector('[name=' + name + ']');
         if (input) input.value = data[name];
       }
     }, configs);
+
+    this.rows = [...document.querySelectorAll('table tbody tr')];
 
     this.initForms();
     this.initButtons();
@@ -74,44 +76,54 @@ class HcodeGrid {
     return JSON.parse(tr.dataset.row);
   }
 
+  btnUpdateClick(e) {
+    // this.fireEvent('beforeUpdateClick', [e]);
+
+    let data = this.getTrData(e);
+
+    for (let name in data) {
+      this.options.onUpdateLoad(this.formUpdate, name, data);
+    }
+
+    this.fireEvent('afterUpdateClick', [e]);
+  }
+
+  btnDeleteClick(e) {
+    this.fireEvent('beforeDeleteClick');
+
+    let data = this.getTrData(e);
+
+    /**
+     * ! para fazermos uma template string passada por parametro (como string), voltar a ser uma template string, aqui precisamos fazer um eval, da nossa string
+     * * mas para a template string funcionar, precisamos agora acrescentar as crases '`'
+     */
+    if (confirm(eval('`' + this.options.deleteMsg + '`'))) {// agora a minha string, esta dentro de um template string, e vou fazer um eval (pegar o valor dela)
+      fetch(eval('`' + this.options.deleteUrl + '`'), {
+        method: 'DELETE'
+      })
+        .then(response => response.json())
+        .then(json => {
+          this.fireEvent('afterDeleteClick');
+        });
+    }
+  }
+
   initButtons() {
-    // UPDATE
-    [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
-      btn.addEventListener('click', e => {
+    this.rows.forEach(row => {
+      [...row.querySelectorAll('.btn')].forEach(btn => {
+        btn.addEventListener('click', e => {
+          // UPDATE
+          if (e.target.classList.contains(this.options.btnUpdate)) {
+            this.btnUpdateClick(e);
 
-        let data = this.getTrData(e);
+          // DELETE
+          } else if (e.target.classList.contains(this.options.btnDelete)) {
+            this.btnDeleteClick(e);
 
-        for (let name in data) {
-          this.options.onUpdateLoad(this.formUpdate, name, data);
-        }
-
-        this.fireEvent('afterUpdateClick', [e]);
-
-      });
-    });
-
-    // DELETE
-    [...document.querySelectorAll(this.options.btnDelete)].forEach(btn => {
-      btn.addEventListener('click', e => {
-
-        this.fireEvent('beforeDeleteClick');
-
-        let data = this.getTrData(e);
-
-        /**
-         * ! para fazermos uma template string passada por parametro (como string), voltar a ser uma template string, aqui precisamos fazer um eval, da nossa string
-         * * mas para a template string funcionar, precisamos agora acrescentar as crases '`'
-         */
-        if (confirm(eval('`' + this.options.deleteMsg + '`'))) {// agora a minha string, esta dentro de um template string, e vou fazer um eval (pegar o valor dela)
-          fetch(eval('`' + this.options.deleteUrl + '`'), {
-            method: 'DELETE'
-          })
-            .then(response => response.json())
-            .then(json => {
-              this.fireEvent('afterDeleteClick');
-            });
-        }
-
+          } else {
+            this.fireEvent('buttonClick', [e.target, this.getTrData(e), e]);
+          }
+        });
       });
     });
   }
