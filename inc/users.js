3614f60ba425113b0bed7930929ac7fa445bd2d5
Done Ribeiro <www.done_@hotmail.com>
2022-03-15 18:43:44 -0300

    users - change password

--- inc/users.js
+++ inc/users.js
@@ -107,6 +107,32 @@ module.exports = {
         }
       });
     });
+  },
+
+  changePassword(req) {
+    return new Promise((resolve, reject) => {
+      if (!req.fields.password) {// se, senha eh vazia
+        reject('Preencha a senha.');
+      } else if (req.fields.password !== req.fields.passwordConfirm) {// se, senhas nao conferem
+        reject('Confirme a senha corretamente.');
+      } else {// senhas validas
+        conn.query(`
+          UPDATE tb_users
+          SET
+            password = ?
+          WHERE id = ?
+        `, [
+          req.fields.password,
+          req.fields.id
+        ], (err, results) => {
+          if (err) {
+            reject(err.message);
+          } else {
+            resolve(results);
+          }
+        });
+      }
+    });
   }
 
 };
--- routes/admin.js
+++ routes/admin.js
@@ -144,6 +144,16 @@ router.post('/users', function (req, res, next) {
   });
 });
 
+router.post('/users/password-change', function (req, res, next) {
+  users.changePassword(req).then(results => {
+    res.send(results);
+  }).catch(err => {
+    res.send({
+      error: err
+    });
+  });
+});
+
 router.delete('/users/:id', function (req, res, next) {
   users.delete(req.params.id).then(results => {
     res.send(results);
