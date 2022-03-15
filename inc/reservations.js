0cd8d5292cdb5c278c60fd62e079486c326f8e4d
Done Ribeiro <www.done_@hotmail.com>
2022-03-14 21:09:45 -0300

    reservations - delete

--- inc/reservations.js
+++ inc/reservations.js
@@ -74,4 +74,21 @@ module.exports = {
     });
   },
 
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      conn.query(`
+        DELETE FROM tb_reservations
+        WHERE id = ?
+      `, [
+        id
+      ], (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+
 };
